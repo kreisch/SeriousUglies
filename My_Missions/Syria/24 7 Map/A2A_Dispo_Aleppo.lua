64357fec1184db9dd2adeb8cd8c2990bb49daed4
Julian Kröhl <julian.kroehl@live.de>
2020-08-14 18:24:32 +0200

    Updated A2ADispatcher Syria

--- My_Missions/Syria/24 7 Map/A2A_Dispo_Aleppo.lua
+++ My_Missions/Syria/24 7 Map/A2A_Dispo_Aleppo.lua
@@ -20,27 +20,28 @@ RED_A2ADispatcher_Aleppo:SetTacticalDisplay( true )
 -- Setup the squadrons.
 -- TODO for Airfields; Moose update pending
 
---RED_A2ADispatcher_Aleppo:SetSquadron( "Kras1", AIRBASE.Caucasus.Krasnodar_Pashkovsky, { "SQ CCCP SU-27" }, 20 )
---RED_CAPZone_Aleppo = ZONE_POLYGON:New( "RU_BORDER_Aleppo_CAP", GROUP:FindByName( "RU_BORDER_Aleppo_CAP" ) )
---RED_A2ADispatcher_Aleppo:SetSquadronCap( "Kras1", 800, 1200, 4000, 8000, RED_CAPZone_Aleppo, 600, 800, 4000, 8000, "BARO" )
---RED_A2ADispatcher_Aleppo:SetSquadronCapInterval( "Kras1", 4, 30, 120, 1 )
+RED_A2ADispatcher_Aleppo:SetSquadron( "RU_CAP_Aleppo_Mig21", "Aleppo", { "RU_CAP_Aleppo_Mig21" }, 20 )
+RED_CAPZone_Aleppo = ZONE_POLYGON:New( "RU_BORDER_Aleppo_CAP", GROUP:FindByName( "RU_BORDER_Aleppo_CAP" ) )
+RED_A2ADispatcher_Aleppo:SetSquadronCap( "RU_CAP_Aleppo_Mig21", 800, 1200, 4000, 8000, RED_CAPZone_Aleppo, 600, 800, 4000, 8000, "BARO" )
+RED_A2ADispatcher_Aleppo:SetSquadronCapInterval( "RU_CAP_Aleppo_Mig21", 4, 30, 120, 1 )
+RED_A2ADispatcher_Aleppo:__Start(1)
 
 
 
 
--------- WORKAROUND STARTS HERE TODO
-
-CapPlane = GROUP:FindByName( "RU_CAP_Aleppo_Mig21" )
-
-PatrolZoneGroup = GROUP:FindByName( "RU_BORDER_Aleppo_GCI" )
-PatrolZone = ZONE_POLYGON:New( "Patrol Zone", PatrolZoneGroup )
-AICapZone = AI_CAP_ZONE:New( PatrolZone, 500, 1000, 500, 600 )
-
-
-EngageZoneGroup = GROUP:FindByName( "RU_BORDER_Aleppo_CAP" )
-CapEngageZone = ZONE_POLYGON:New( "Engage Zone", EngageZoneGroup )
-
-AICapZone:SetControllable( CapPlane )
-AICapZone:SetEngageZone( CapEngageZone ) -- Set the Engage Zone. The AI will only engage when the bogeys are within the CapEngageZone.
-
-AICapZone:__Start( 1 ) -- They should statup, and start patrolling in the PatrolZone.
+---------- WORKAROUND STARTS HERE TODO
+--
+--CapPlane = GROUP:FindByName( "RU_CAP_Aleppo_Mig21" )
+--
+--PatrolZoneGroup = GROUP:FindByName( "RU_BORDER_Aleppo_GCI" )
+--PatrolZone = ZONE_POLYGON:New( "Patrol Zone", PatrolZoneGroup )
+--AICapZone = AI_CAP_ZONE:New( PatrolZone, 500, 1000, 500, 600 )
+--
+--
+--EngageZoneGroup = GROUP:FindByName( "RU_BORDER_Aleppo_CAP" )
+--CapEngageZone = ZONE_POLYGON:New( "Engage Zone", EngageZoneGroup )
+--
+--AICapZone:SetControllable( CapPlane )
+--AICapZone:SetEngageZone( CapEngageZone ) -- Set the Engage Zone. The AI will only engage when the bogeys are within the CapEngageZone.
+--
+--AICapZone:__Start( 1 ) -- They should statup, and start patrolling in the PatrolZone.
