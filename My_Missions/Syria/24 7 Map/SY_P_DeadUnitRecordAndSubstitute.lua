16d68ebe40c55d4e69ba2cad9df43061cac03fec
UglySkyfire <69476075+UglySkyfire@users.noreply.github.com>
2020-08-26 09:06:06 +0200

    Cleanup of name and tmp fix for slingload
    * Renamed persistence files to match pattern
    * Set real slingload to false as it's currently not working

---
+++ My_Missions/Syria/24 7 Map/SY_P_DeadUnitRecordAndSubstitute.lua
@@ -0,0 +1,319 @@
+-- Persistence skript by [ugly] Skyfire, based on a mission by Surrexen
+-- https://www.digitalcombatsimulator.com/en/files/?CREATED_BY=Surrexen&set_filter=Y
+
+
+
+----------------------------------------------------------
+-- Default values for the script
+----------------------------------------------------------
+-- Seconds between each table write
+SaveDeadListIntervall = 600
+
+-- Print some more text during script execution
+ShowDebugText = false
+
+-- When a unit with this name is found, it is placed instead of the dead removed units
+-- so the area does not look that empty
+-- UnitExchangeTemplateName = "__Substitute_Soft"
+-- ExchangeOldUnit = true
+
+-- The amount of seconds each message is visible
+MessageDefaultShowTime = 10
+----------------------------------------------------------
+
+-- Global default variable needed later. Don't set any values here!
+PathToUserData = ""
+UnitsFileName = ""
+UnitsFilePath = ""
+StaticsFilePath = ""
+DeadUnitsList = SET_UNIT:New()
+
+PersistenceAvailable = true
+
+function UglyMessageToAll(_theNeatMessage)
+  trigger.action.outText(_theNeatMessage, MessageDefaultShowTime)
+end
+
+function UglyPrintDebugText(_theNeatMessage)
+  if ShowDebugText then
+    trigger.action.outText(_theNeatMessage, MessageDefaultShowTime)
+  end
+end
+
+function uglyCheckPersistence()
+  if io and lfs then
+    UglyPrintDebugText("lfs and io available")
+
+    if lfs.attributes(PathToUserData) then
+      UglyPrintDebugText(PathToUserData.." ok")
+      PersistencePath = PathToUserData.."\\Persistence"
+
+      if lfs.attributes(PersistencePath) then
+        UglyPrintDebugText(PersistencePath.." ok")
+      else
+        lfs.mkdir(PersistencePath)
+      end
+
+      UnitsFilePath = PersistencePath.."\\"..UnitsFileName
+      StaticsFilePath = PersistencePath.."\\"..StaticsFileName
+
+      UglyPrintDebugText("Path to Userdata available, ready to load: "..UnitsFilePath)
+      UglyPrintDebugText("Path to Userdata available, ready to load: "..StaticsFilePath)
+      return true
+    else
+      UglyPrintDebugText("Path to Userdata not available: "..PathToUserData)
+      UglyPrintDebugText("Path to Userdata not available: "..StaticsFilePath)
+      return false
+    end
+  else
+    UglyPrintDebugText("lfs and io not available -> MissionScripting.lua has to be modified to enable persistence!", 3)
+	PersistenceAvailable = false
+    return false
+  end
+end
+
+function IntegratedbasicSerialize(s)
+    if s == nil then
+      return "\"\""
+    else
+      if ((type(s) == 'number') or (type(s) == 'boolean') or (type(s) == 'function') or (type(s) == 'table') or (type(s) == 'userdata') ) then
+        return tostring(s)
+      elseif type(s) == 'string' then
+        return string.format('%q', s)
+      end
+    end
+end
+
+-- imported slmod.serializeWithCycles (Speed)
+function IntegratedserializeWithCycles(name, value, saved)
+  local basicSerialize = function (o)
+    if type(o) == "number" then
+      return tostring(o)
+    elseif type(o) == "boolean" then
+      return tostring(o)
+    else -- assume it is a string
+      return IntegratedbasicSerialize(o)
+    end
+  end
+
+  local t_str = {}
+  saved = saved or {}       -- initial value
+  if ((type(value) == 'string') or (type(value) == 'number') or (type(value) == 'table') or (type(value) == 'boolean')) then
+    table.insert(t_str, name .. " = ")
+      if type(value) == "number" or type(value) == "string" or type(value) == "boolean" then
+        table.insert(t_str, basicSerialize(value) ..  "\n")
+      else
+        if saved[value] then    -- value already saved?
+          table.insert(t_str, saved[value] .. "\n")
+        else
+          saved[value] = name   -- save name for next time
+          table.insert(t_str, "{}\n")
+            for k,v in pairs(value) do      -- save its fields
+              local fieldname = string.format("%s[%s]", name, basicSerialize(k))
+              table.insert(t_str, IntegratedserializeWithCycles(fieldname, v, saved))
+            end
+        end
+      end
+    return table.concat(t_str)
+  else
+    return ""
+  end
+end
+
+function file_exists(name) --check if the file already exists for writing
+  if lfs.attributes(name) then
+    return true
+    else
+    return false end
+end
+
+function writemission(data, file)--Function for saving to file (commonly found)
+  File = io.open(file, "w")
+  File:write(data)
+  File:close()
+end
+
+--////SAVE UNITS FUNCTION
+function SEF_SaveUglyUnitDeadList(timeloop, time)
+  UglyPrintDebugText("Unit progress will be saved...")
+  IntermentMissionStr = IntegratedserializeWithCycles("UglyUnitDeadList", UglyUnitDeadList)
+  writemission(IntermentMissionStr, UnitsFilePath)
+--  UglyPrintDebugText("SEF_SaveUglyUnitDeadListNoArgs!"..IntermentMissionStr)
+  UglyPrintDebugText("Unit progress has been saved!")
+  return time + SaveDeadListIntervall
+end
+
+function SEF_SaveUglyUnitDeadListNoArgs()
+  IntermentMissionStr = IntegratedserializeWithCycles("UglyUnitDeadList", UglyUnitDeadList)
+  UglyPrintDebugText("SEF_SaveUglyUnitDeadListNoArgs!"..IntermentMissionStr)
+  writemission(IntermentMissionStr, UnitsFilePath)
+end
+
+--////SAVE STATICS FUNCTION
+function SEF_SaveUglyStaticDeadList(timeloop, time)
+  UglyPrintDebugText("Static progress will be saved...")
+  IntermentMissionStrStatic = IntegratedserializeWithCycles("UglyStaticDeadList", UglyStaticDeadList)
+  writemission(IntermentMissionStrStatic, StaticsFilePath)
+  UglyPrintDebugText("Static progress has been saved!")
+  return time + SaveDeadListIntervall
+end
+
+function SEF_SaveUglyStaticDeadListNoArgs()
+	IntermentMissionStrStatic = IntegratedserializeWithCycles("UglyStaticDeadList", UglyStaticDeadList)
+	writemission(IntermentMissionStrStatic, StaticsFilePath)	
+end
+
+function UglySubstituteAndKillUnit(SubstituteUnit)
+	if ( SubstituteUnit ~= nil ) then
+    local DeadUnit = UNIT:FindByName(SubstituteUnit["name"])
+    if ( DeadUnit ~= nil ) then
+		  DeadUnit:Destroy()
+		end
+
+		coalition.addStaticObject(SubstituteUnit["countryID"], SubstituteUnit)
+		UglyPrintDebugText("Added new static!!! "..SubstituteUnit["countryID"])
+	end
+end
+
+function UglySubstituteAndKillStatic(SubstituteStatic)
+	if ( SubstituteStatic ~= nil ) then
+    local DeadStatic = STATIC:FindByName(SubstituteStatic["name"], false)
+    if ( DeadStatic ~= nil ) then
+		  DeadStatic:Destroy()
+		end
+
+		coalition.addStaticObject(SubstituteStatic["countryID"], SubstituteStatic)
+		UglyPrintDebugText("Added new static!!! "..SubstituteStatic["countryID"])
+	end
+end
+
+-------------------------------------------------------------------------------------------------------------------------------------
+--//// MAIN persistence function
+
+function StartUglyPersistence(_pathToUserData, _unitsFileName, _staticsFileName)
+  UglyPrintDebugText("Trying to start Ugly Persistence!")
+
+  PathToUserData = _pathToUserData
+  UnitsFileName = _unitsFileName
+  StaticsFileName = _staticsFileName
+
+  local SEFDeletedUnitCount = 0
+  local SEFDeletedStaticCount = 0
+
+--  DeadUnitsList:FilterCoalitions("red"):FilterCategories("ground"):FilterActive(true):FilterStart()
+  DeadUnitsList:HandleEvent(EVENTS.Dead)
+
+  --////LOAD UNITS
+  
+  if uglyCheckPersistence() then
+    if file_exists(UnitsFilePath) then
+--    if false then
+      UglyPrintDebugText("Loading: "..UnitsFilePath)
+
+      dofile(UnitsFilePath)
+
+      UglyPrintDebugText("Unit Table Length Is "..#UglyUnitDeadList)
+
+      for i = 1, #UglyUnitDeadList do
+        UglyPrintDebugText("Dead Unit Element "..i.." Is "..UglyUnitDeadList[i]["name"])
+
+        UglySubstituteAndKillUnit(UglyUnitDeadList[i])
+        SEFDeletedUnitCount = SEFDeletedUnitCount + 1
+      end
+    else
+      UglyPrintDebugText("Creating data for: "..UnitsFilePath)
+      UglyUnitDeadList = {}
+    end
+
+    UglyMessageToAll("Persistent World Functions Have Removed "..SEFDeletedUnitCount.." Units")
+
+    --////LOAD STATICS
+    if file_exists(StaticsFilePath) then
+      UglyPrintDebugText("Loading: "..StaticsFilePath)
+
+  	  dofile(StaticsFilePath)
+		
+      UglyPrintDebugText("Static Table Length Is "..#UglyStaticDeadList)
+	
+  	  for i = 1, #UglyStaticDeadList do
+        UglySubstituteAndKillStatic(UglyStaticDeadList[i])
+        SEFDeletedStaticCount = SEFDeletedStaticCount + 1
+      end	
+    else
+      UglyPrintDebugText("Creating data for: "..StaticsFileName)
+  	  UglyStaticDeadList = {}
+	    StaticIntermentTableLength = 0	
+    end
+
+    UglyMessageToAll("Persistent World Functions Have Removed "..SEFDeletedStaticCount.." Statics")
+
+--SCHEDULE
+    timer.scheduleFunction(SEF_SaveUglyUnitDeadList, {}, timer.getTime() + SaveDeadListIntervall)
+    timer.scheduleFunction(SEF_SaveUglyStaticDeadList, {}, timer.getTime() + SaveDeadListIntervall)
+    UglyMessageToAll("Ugly Persistence is active. Unit list will be stored every "..SaveDeadListIntervall.." seconds.")
+    ---------------------------------------------------------------------------------------------------------------------------------------------------
+  end
+end
+
+function dumpToTable(theTable, DEADUNIT)
+  local CountryID = DEADUNIT:GetCountry()
+  local CountryName = _DATABASE.COUNTRY_NAME[CountryID]
+
+  local deadObj = {
+    ["country"] = CountryName, 
+    ["countryID"] = CountryID, 
+    ["type"]=DEADUNIT:GetTypeName(),
+    ["coalition"]=DEADUNIT:GetCoalitionName(),
+    ["category"]=DEADUNIT:GetCategory(),
+    ["y"]=DEADUNIT:GetPointVec2():GetY(),
+    ["x"]=DEADUNIT:GetPointVec2():GetX(),
+    ["name"]=DEADUNIT:GetName(),
+    ["heading"]=DEADUNIT:GetHeading(),
+    ["shape_name"]=DEADUNIT:GetTypeName(),
+    ["dead"] = true,
+  }
+
+  table.insert(theTable, deadObj)
+end
+
+function DeadUnitsList:OnEventDead(EventData)
+
+  local DEADUNIT = EventData.IniUnit
+  local DEADUNITNAME = EventData.IniDCSUnitName
+  local DEADUNITCOALITION = EventData.IniCoalition
+  local DEADUNITOBJECTCATEGORY = EventData.IniObjectCategory  -- 1 UNIT / 2 WEAPON / 3 STATIC / 4 BASE / 5 SCENERY / 6 CARGO
+  local DEADUNITCATEGORY = EventData.IniCategory        -- 0 AIRPLANE / 1 HELICOPTER / 2 GROUND_UNIT / 3 SHIP / 4 STRUCTURE
+
+    --Debug Zone
+--    UglyPrintDebugText("Dead Unit Name: "..DEADUNITNAME)
+--    UglyPrintDebugText("Dead Unit Coalition: "..DEADUNITCOALITION)
+--    UglyPrintDebugText("Dead Unit Category: "..DEADUNITCATEGORY)
+--    UglyPrintDebugText("Dead Unit Object Category: "..DEADUNITOBJECTCATEGORY)
+
+  
+  if ( DEADUNITOBJECTCATEGORY == 1 ) then -- UNIT
+    if ( DEADUNITCATEGORY == 2 or DEADUNITCATEGORY == 3 ) then -- GROUND_UNIT or SHIP
+    UglyPrintDebugText("PERSISTED! Dead Unit ["..DEADUNITNAME.."]")
+    dumpToTable(UglyUnitDeadList, DEADUNIT)
+    end
+  elseif ( DEADUNITOBJECTCATEGORY == 3 ) then -- STATIC
+    UglyPrintDebugText("PERSISTED! Dead Static ["..DEADUNITNAME.."]")
+    dumpToTable(UglyStaticDeadList, DEADUNIT)
+  end		
+
+  local DEADDCSUNIT = EventData.IniDCSUnit
+  DEADDCSUNIT:destroy()
+end
+
+---------------------------------------------------------------------------------------------------------------------------------------------------
+--//// Do persistence
+
+--PathToUserData = "C:\\Users\\Phil\\Saved Games\\DCS.openbeta"  --Skyfire local
+--PathToUserData = "C:\\Users\\uglys\\Saved Games\\DCS.server"   --Skyfire server
+--PathToUserData = "C:\\Users\\Administrator\\Saved Games\\DCS.openbeta_server"  --Chris Server
+--UnitsFileName = "UglyUnitData.lua"
+
+UglyMessageToAll("MessageToAll: Trying to start Ugly Persistence!")
+StartUglyPersistence("C:\\temp", "UglyUnitDeadListSyria247.lua", "UglyStaticsDeadListSyria247.lua")
+UglyMessageToAll("MessageToAll: Ugly Persistence started!")
+
---
+++ My_Missions/Syria/24 7 Map/SY_P_NewUnitsAndStaticsPos.lua
@@ -0,0 +1,228 @@
+-- Uses the Simple Group Saving by Pikey May 2019
+-- Usage of this script should credit the following contributors:
+-- Pikey, 
+-- Speed & Grimes for their work on Serialising tables, included below,
+-- FlightControl for MOOSE (Required)
+
+-- Heavily modified by [ugly] Skyfire
+ 
+ -----------------------------------
+ --Configurable for user:
+ SaveIntervall = 600 --how many seconds between each check of all the statics.
+ 
+ local unitsPosFile = "C:\\temp\\Persistence\\UglyUnitPositionsSyria247.lua" --edit this to represent your own (DCS cant write to different disks)
+
+ 
+ -----------------------------------
+ --Do not edit below here
+ -----------------------------------
+ local version = "1.0"
+ 
+ function IntegratedbasicSerialize(s)
+    if s == nil then
+      return "\"\""
+    else
+      if ((type(s) == 'number') or (type(s) == 'boolean') or (type(s) == 'function') or (type(s) == 'table') or (type(s) == 'userdata') ) then
+        return tostring(s)
+      elseif type(s) == 'string' then
+        return string.format('%q', s)
+      end
+    end
+  end
+  
+-- imported slmod.serializeWithCycles (Speed)
+  function IntegratedserializeWithCycles(name, value, saved)
+    local basicSerialize = function (o)
+      if type(o) == "number" then
+        return tostring(o)
+      elseif type(o) == "boolean" then
+        return tostring(o)
+      else -- assume it is a string
+        return IntegratedbasicSerialize(o)
+      end
+    end
+
+    local t_str = {}
+    saved = saved or {}       -- initial value
+    if ((type(value) == 'string') or (type(value) == 'number') or (type(value) == 'table') or (type(value) == 'boolean')) then
+      table.insert(t_str, name .. " = ")
+      if type(value) == "number" or type(value) == "string" or type(value) == "boolean" then
+        table.insert(t_str, basicSerialize(value) ..  "\n")
+      else
+
+        if saved[value] then    -- value already saved?
+          table.insert(t_str, saved[value] .. "\n")
+        else
+          saved[value] = name   -- save name for next time
+          table.insert(t_str, "{}\n")
+          for k,v in pairs(value) do      -- save its fields
+            local fieldname = string.format("%s[%s]", name, basicSerialize(k))
+            table.insert(t_str, IntegratedserializeWithCycles(fieldname, v, saved))
+          end
+        end
+      end
+      return table.concat(t_str)
+    else
+      return ""
+    end
+  end
+
+function file_exists(name) --check if the file already exists for writing
+    if lfs.attributes(name) then
+    return true
+    else
+    return false end 
+end
+
+function writemission(data, file)--Function for saving to file (commonly found)
+  File = io.open(file, "w")
+  File:write(data)
+  File:close()
+end
+
+--SCRIPT START
+env.info("Units and Static position persistence " .. version)
+
+function LoadOldGroups()
+  if file_exists(unitsPosFile) then --Script has been run before, so we need to load the save
+    env.info("Existing database, loading from File.")
+  --    AllGroups:ForEachGroup(function (grp)
+  --      grp:Destroy()
+  --    end)
+
+    dofile(unitsPosFile)
+    tempTable={}
+    Spawn={}
+  --RUN THROUGH THE KEYS IN THE TABLE (GROUPS)
+    for k,v in pairs (SaveUnits) do
+      units={}
+  --RUN THROUGH THE UNITS IN EACH GROUP
+      for i= 1, #(SaveUnits[k]["units"]) do 
+        tempTable =
+        { 
+          ["type"]=SaveUnits[k]["units"][i]["type"],
+          ["transportable"]= {["randomTransportable"] = false,}, 
+          --["unitId"]=9000,used to generate ID's here but no longer doing that since DCS seems to handle it
+          ["skill"]=SaveUnits[k]["units"][i]["skill"],
+          ["y"]=SaveUnits[k]["units"][i]["y"] ,
+          ["x"]=SaveUnits[k]["units"][i]["x"] ,
+          ["name"]=SaveUnits[k]["units"][i]["name"],
+          ["heading"]=SaveUnits[k]["units"][i]["heading"],
+          ["playerCanDrive"]=true,  --hardcoded but easily changed.  
+        }
+
+        table.insert(units,tempTable)
+
+        local DeleteUnit = UNIT:FindByName( SaveUnits[k]["units"][i]["name"] )
+  
+        if DeleteUnit ~= nil then
+          DeleteUnit:Destroy()	
+        end
+      end --end unit for loop
+
+      groupData = 
+      {
+        ["visible"] = true,
+        --["lateActivation"] = false,
+        ["tasks"] = {}, -- end of ["tasks"]
+        ["uncontrollable"] = false,
+        ["task"] = "Ground Nothing",
+        --["taskSelected"] = true,
+        --["route"] = 
+        --{ 
+        --["spans"] = {},
+        --["points"]= {}
+        -- },-- end of ["spans"] 
+        --["groupId"] = 9000 + _count,
+        ["hidden"] = false,
+        ["units"] = units,
+        ["y"] = SaveUnits[k]["y"],
+        ["x"] = SaveUnits[k]["x"],
+        ["name"] = SaveUnits[k]["name"],
+        --["start_time"] = 0,
+      } 
+
+      coalition.addGroup(SaveUnits[k]["CountryID"], SaveUnits[k]["CategoryID"], groupData)
+      groupData = {}
+    end --end Group for loop
+
+--    AllGroups = SET_GROUP:New():FilterCategories("ground"):FilterActive(true):FilterStart()
+
+  else --Save File does not exist we start a fresh table, no spawns needed
+    SaveUnits={}
+--    AllGroups = SET_GROUP:New():FilterCategories("ground"):FilterActive(true):FilterStart()
+  end
+end
+
+local function starts_with(str, start)
+  return str:sub(1, #start) == start
+end
+
+LoadOldGroups()
+
+AllGroups = SET_GROUP:New():FilterCategories("ground"):FilterActive(true):FilterStart()
+
+--THE SAVING SCHEDULE
+SCHEDULER:New( nil, function()
+
+  RelevantGroups = SET_GROUP:New()
+
+  AllGroups:ForEachGroupAlive(function (grp)
+    local DCSgroup = Group.getByName(grp:GetName() )
+    if starts_with(grp:GetName(), "S_") then
+--      env.info("Skipping Unit: "..grp:GetName())
+--      trigger.action.outText("Skipping Unit: "..grp:GetName(), 10)
+    else
+      RelevantGroups:AddGroup(grp)
+--      env.info("Using Unit: "..grp:GetName())
+--      trigger.action.outText("Using Unit: "..grp:GetName(), 10)
+    end
+  end)
+
+  RelevantGroups:ForEachGroup(function (grp)
+    local DCSgroup = Group.getByName(grp:GetName() )
+  	local size = DCSgroup:getSize()
+
+    _unittable={}
+
+    for i = 1, size do
+	  local tmpTable =
+		{   
+		  ["type"]=grp:GetUnit(i):GetTypeName(),
+	      ["transportable"]=true,
+	      ["unitID"]=grp:GetUnit(i):GetID(),
+	      ["skill"]="Average",
+	      ["y"]=grp:GetUnit(i):GetVec2().y,
+	      ["x"]=grp:GetUnit(i):GetVec2().x,
+	      ["name"]=grp:GetUnit(i):GetName(),
+	      ["playerCanDrive"]=true,
+	      ["heading"]=grp:GetUnit(i):GetHeading(),
+	    }
+	  table.insert(_unittable,tmpTable) --add units to a temporary table
+    end
+
+    SaveUnits[grp:GetName()] =
+    {
+      ["CountryID"]=grp:GetCountry(),
+      ["SpawnCoalitionID"]=grp:GetCountry(),
+      ["tasks"]={}, --grp:GetTaskMission(), --wrong gives the whole thing
+      ["CategoryID"]=grp:GetCategory(),
+      ["task"]="Ground Nothing",
+      ["route"]={}, -- grp:GetTaskRoute(),
+      ["groupId"]=grp:GetID(),
+--["SpawnCategoryID"]=grp:GetCategory(),
+      ["units"]= _unittable,
+      ["y"]=grp:GetVec2().y, 
+      ["x"]=grp:GetVec2().x,
+      ["name"]=grp:GetName(),
+      ["start_time"]=0,
+      ["CoalitionID"]=grp:GetCoalition(),
+      ["SpawnCountryID"]=grp:GetCoalition(),
+    }
+  end)
+
+  newMissionStr = IntegratedserializeWithCycles("SaveUnits",SaveUnits) --save the Table as a serialised type with key SaveUnits
+  writemission(newMissionStr, unitsPosFile)--write the file from the above to SaveUnits.lua
+  SaveUnits={}--clear the table for a new write.
+--env.info("Data saved.")
+end, {}, 10, SaveIntervall)
