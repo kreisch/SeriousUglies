7b5165e0eef2ada154a3cd8cb55e52403a277f16
UglySkyfire <69476075+UglySkyfire@users.noreply.github.com>
2020-09-03 11:30:31 +0200

    Added CSAR restoring

--- My_Missions/Syria/24 7 Map/SY_P_RecreateCTLDData.lua
+++ My_Missions/Syria/24 7 Map/SY_P_RecreateCTLDData.lua
@@ -12,6 +12,8 @@ local RestoreGroups = SET_GROUP:New():FilterActive(true):FilterStart()
 
 RestoreGroups:ForEachGroupAlive(function (grp)
   for i = 1, #grp:GetUnits() do
+
+	-- Check for CTLD JTAC
     if starts_with(grp:GetUnit(i):GetName(), "Unpacked Hummer") then
       local _code = table.remove(ctld.jtacGeneratedLaserCodes, 1)
       --put to the end
@@ -19,6 +21,26 @@ RestoreGroups:ForEachGroupAlive(function (grp)
 
       ctld.JTACAutoLase(grp:GetName(), _code)
       trigger.action.outText("Restoring JTAC status of: "..grp:GetName()..", with laser code: ".._code, 10)
+
+	  -- Check for CSAR Guy
+	  elseif starts_with(grp:GetName(), "Downed Pilot") then
+
+      local _downedGroup = grp:GetDCSObject()
+      local _downedUnit = grp:GetDCSUnit(i)
+
+      trigger.action.setGroupAIOff(_downedGroup)
+      local _freq = csar.generateADFFrequency()
+
+      csar.addBeaconToGroup(_downedGroup:getName(), _freq)
+
+      -- Generate DESCRIPTION text
+      local _text = "Pilot of " .. _downedUnit:getName() .. " - " .. _downedUnit:getTypeName()
+
+      csar.woundedGroups[_downedGroup:getName()] = { side = _downedGroup:getCoalition(), originalUnit = _downedUnit:getName(), frequency = _freq, desc = _text, player = nil }
+
+      csar.initSARForPilot(_downedGroup, _freq)
+      csar.addSpecialParametersToGroup(_downedGroup)
+      trigger.action.outText("Restoring downed pilot: "..grp:GetName(), 10)
     end
   end
 end
