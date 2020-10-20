94ce734d53d692bf0c5e6b6540af53e136c2815b
UglySkyfire <69476075+UglySkyfire@users.noreply.github.com>
2020-10-20 13:18:47 +0200

    Create Ugly_FrameworkConfig_Syria247.lua

---
+++ My_Missions/Syria/24 7 Map/Ugly_FrameworkConfig_Syria247.lua
@@ -0,0 +1,29 @@
+---------------------------------------------------------------------------------------------------
+-- This file is just the configuration for the Ugly-Framework. 
+-- Any parameter can be set directly in the Ugly_Framwork.lua, 
+-- but handling in multiple mission is much easier with a separate config file.
+
+-- Remember to load the Ugly_Framework first, then set any config and finally start the framework
+
+
+assert(Ugly ~= nil, "\n\n** HEY MISSION-DESIGNER! **\n\nThe Ugly Framework has not been loaded!\n\nMake sure Ugly_Framework is running\n*before* running this script!\n")
+
+-- The global save interval
+Ugly.saveInterval = 666
+
+-- If true, print any debug text
+Ugly.showDebugText = false
+
+-- How long in seconds is the message displayed
+Ugly.messageShowTime = 10
+
+Ugly.AutoRecceMarkerPrefix = "M_" -- if "" no marker is created
+-- not in use currently Ugly.KeepDeadRecceMarker = true -- if "" no marker is created 
+
+Ugly.PathToUserData = "C:\\temp" -- A directory "Persistence" is automatically added to the base directory
+Ugly.MissionSuffix = "Syria247"
+
+Ugly.ExportMapIntervall = 5
+Ugly.exportRedUnits = true
+
+local LiveMapBaseDirectory = "C:\\DCS-WebMap\\Serious Uglies\\02 Maps Missions Server\\98 Server Admin\\Syria-Livemap\\"
