447e5a80dff63af187c5b4a7a1df2dc93d35d168
UglySkyfire <69476075+UglySkyfire@users.noreply.github.com>
2022-09-08 13:08:47 +0200

    Added respawn before taking over task

--- My_Missions/Syria/OperationElefant/_ElefantScriptLoader.lua
+++ My_Missions/Syria/OperationElefant/_ElefantScriptLoader.lua
@@ -9,8 +9,9 @@ assert(loadfile(baseDir .. "ElefantCSAR.lua"))()
 assert(loadfile(baseDir .. "ElefantCTLD.lua"))()
 assert(loadfile(baseDir .. "Elefant_Airboss.lua"))()
 assert(loadfile(baseDir .. "ElefantTankerRespawnConfig.lua"))()
-assert(loadfile(baseDir .. "ElefantTestAI.lua"))()
+-- Factory must be loaded before control zone handling. Zone handling needs to register groups at the factory.
 assert(loadfile(baseDir .. "RespawnFactory.lua"))()
+assert(loadfile(baseDir .. "ElefantTestAI.lua"))()
 
 trigger.action.outText("All scripts dynamically loaded!", 10, false)
 
