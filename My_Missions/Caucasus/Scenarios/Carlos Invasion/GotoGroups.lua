4bfe8fe5ddbf2706e713c9f4bf18e61cb642eca1
UglySkyfire <69476075+UglySkyfire@users.noreply.github.com>
2022-10-26 11:32:25 +0200

    Added reroute scheduling

--- My_Missions/Caucasus/Scenarios/Carlos Invasion/GotoGroups.lua
+++ My_Missions/Caucasus/Scenarios/Carlos Invasion/GotoGroups.lua
@@ -13,11 +13,60 @@
 
 local zonePrefix = "GOTO_"
 local gotoFrequency = 600 -- in seconds - 600s equals 10min
-
+local numberPerStep = 1 -- how many units are moved each iteration
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- helper queue structure
+
+GotoList = {}
+local theList = {}
+
+function GotoList.new ()
+  return {first = 0, last = -1}
+end
+
+--[[
+function GotoList.pushleft (list, value)
+  local first = list.first - 1
+  list.first = first
+  list[first] = value
+end
+]]
+
+function GotoList.pushright (list, value)
+  local last = list.last + 1
+  list.last = last
+  list[last] = value
+end
+
+function GotoList.popleft (list)
+  local first = list.first
+
+  if first > list.last then 
+    return nil
+  end
+
+  local value = list[first]
+  list[first] = nil        -- to allow garbage collection
+  list.first = first + 1
+  return value
+end
+
+--[[
+function GotoList.popright (list)
+  local last = list.last
+
+  if list.first > last then
+    return nil
+  end
+
+  local value = list[last]
+  list[last] = nil         -- to allow garbage collection
+  list.last = last - 1
+  return value
+end
+]]
 
 -- Call this function for each zone that should be observed
-
 function CheckGotosForZone(_zone)
   env.info("CheckGotosForZone: " .. _zone:GetName())
 
@@ -25,13 +74,37 @@ function CheckGotosForZone(_zone)
 
   SetGroups:ForEachGroupNotInZone(_zone, function(groupToMove)
     env.info("Rerouting group: " .. groupToMove:GetName())
-    local spawnVec2 = _zone:GetRandomVec2()
-    groupToMove:RouteGroundOnRoad(COORDINATE:NewFromVec2(spawnVec2), 30)--, 1, "Vee", respawnAtLastWP)
+    local destVec2 = _zone:GetRandomVec2()
+    local gotoData = {group = groupToMove, dest = destVec2}
+    GotoList.pushright(theList, gotoData)
   end
   )
 
   TIMER:New(CheckGotosForZone, _zone):Start(gotoFrequency)
 end
+
+
+function ExecuteAllGotos()
+  env.info("Start ExecuteAllGotos...")
+  --  local gotoData = {group = groupToMove, dest = destVec2}
+
+  for i = 1, numberPerStep do
+    local gotoData = GotoList.popleft(theList)
+    if gotoData then
+      if gotoData.group ~= nil then
+        env.info("ExecuteAllGoto for: " .. gotoData.group:GetName())
+        gotoData.group:RouteGroundOnRoad(COORDINATE:NewFromVec2(gotoData.dest), 50)--, 1, "Vee", respawnAtLastWP)
+      end
+    else
+      break
+    end
+  end
+
+  env.info("End ExecuteAllGotos...")
+  TIMER:New(ExecuteAllGotos):Start(1)
+end
+
+theList = GotoList.new()
 
 -- init for all GoTo zones
 local SetZones = SET_ZONE:New():FilterPrefixes(zonePrefix):FilterOnce()
@@ -40,3 +113,5 @@ SetZones:ForEachZone(function(zoneToMoveTo)
   CheckGotosForZone(zoneToMoveTo)
 end
 )
+
+ExecuteAllGotos()
